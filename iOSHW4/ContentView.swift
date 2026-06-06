4eb88bc53d4ce2d29b55a4149e4c4bcdfb42ec5c
jose <josersanchez0117@gmail.com>
2026-06-05 19:43:47 -0500

    Initial Commit

---
+++ iOSHW4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSHW4
+//
+//  Created by jose on 6/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSHW4/iOSHW4App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSHW4App.swift
+//  iOSHW4
+//
+//  Created by jose on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSHW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
